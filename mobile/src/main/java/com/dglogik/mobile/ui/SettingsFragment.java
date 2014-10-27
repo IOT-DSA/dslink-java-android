21b660ce49a16d0e114d268f507740dce5ade9d5
Kenneth Endfinger <kaendfinger@gmail.com>
2014-10-27 14:34:00 -0400

    Added Working Preferences System

--- mobile/src/main/java/com/dglogik/mobile/ui/SettingsFragment.java
+++ mobile/src/main/java/com/dglogik/mobile/ui/SettingsFragment.java
@@ -4,23 +4,18 @@ import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
-import android.preference.PreferenceScreen;
 
 import com.dglogik.mobile.DGConstants;
+import com.dglogik.mobile.R;
 
 public class SettingsFragment extends PreferenceFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        PreferenceManager preferenceManager = getPreferenceManager();
-        PreferenceScreen screen = preferenceManager.createPreferenceScreen(getActivity().getApplicationContext());
-        PreferenceCategory nodesGroup = new PreferenceCategory(getActivity().getApplicationContext());
-        nodesGroup.setTitle("Nodes");
+        addPreferencesFromResource(R.xml.preferences);
+        PreferenceCategory nodesCategory = (PreferenceCategory) findPreference("nodes");
         for (Preference preference : DGConstants.createNodePreferences(getActivity().getApplicationContext())) {
-            nodesGroup.addPreference(preference);
+            nodesCategory.addPreference(preference);
         }
-        screen.addPreference(nodesGroup);
-        setPreferenceScreen(screen);
     }
 }
