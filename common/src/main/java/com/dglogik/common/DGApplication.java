66cd568595ef4d3756fe371335fc664e23b025e6
Kenneth Endfinger <kaendfinger@gmail.com>
2014-11-23 14:12:24 -0500

    Bug Fix

--- common/src/main/java/com/dglogik/common/DGApplication.java
+++ common/src/main/java/com/dglogik/common/DGApplication.java
@@ -3,6 +3,9 @@ package com.dglogik.common;
 import android.app.Application;
 import android.support.multidex.MultiDex;
 
+import android.os.*;
+import android.app.*;
+import android.content.*;
 import org.acra.ACRA;
 import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
